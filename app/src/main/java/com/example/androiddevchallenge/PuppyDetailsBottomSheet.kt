bacdbc7cfb8fde3161d67e328b3b7ea09e6d2f87
Anastasia Finogenova <f112583@heb.com>
2021-03-01 14:46:27 -0600

    fixed build issue

--- app/src/main/java/com/example/androiddevchallenge/PuppyDetailsBottomSheet.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyDetailsBottomSheet.kt
@@ -35,8 +35,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
